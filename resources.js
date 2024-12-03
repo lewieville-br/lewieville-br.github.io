abf0674aea5c0be28e1fd977dce5e4dee4901281
lewieville-br <lewis.miller@svu.edu>
2024-12-03 16:36:19 -0500

    Update resources.js

--- resources.js
+++ resources.js
@@ -1,33 +1,43 @@
 function showContent(section) {
     var container = document.getElementsByClassName('container')[0];
     var sections = container.children;
-    for (var i = 0; i < sections.length; i++) {
+
+    var i = 0;
+    while (i < sections.length) {
         sections[i].style.display = 'none';
+        i++;
     }
+
     var targetSection = document.getElementsByClassName(section + 'Text')[0];
-    if (targetSection) {
-        targetSection.style.display = 'block';
-    }
+    targetSection.style.display = 'block';
+}
+
+function showAbout() {
+    showContent('about');
+}
+
+function showServices() {
+    showContent('service');
+}
+
+function showContact() {
+    showContent('contact');
+}
+
+function showFAQ() {
+    showContent('faq');
 }
 
 var aboutButton = document.getElementsByClassName('aboutme')[0];
-aboutButton.onclick = function() {
-    showContent('about');
-};
+aboutButton.onclick = showAbout;
 
 var servicesButton = document.getElementsByClassName('services')[0];
-servicesButton.onclick = function() {
-    showContent('service');
-};
+servicesButton.onclick = showServices;
 
 var contactButton = document.getElementsByClassName('contact')[0];
-contactButton.onclick = function() {
-    showContent('contact');
-};
+contactButton.onclick = showContact;
 
 var faqButton = document.getElementsByClassName('faq')[0];
-faqButton.onclick = function() {
-    showContent('faq');
-};
+faqButton.onclick = showFAQ;
 
 showContent('about');
