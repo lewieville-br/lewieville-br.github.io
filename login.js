58529dd1fac055b689a327b0909ac19f5c5eff21
lewieville <lewis.miller@svu.edu>
2024-12-06 15:06:22 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,6 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    alert('Welcome! Use promo code "LOGIN" for 15% off.');
+    username = document.getElementById("username").value;
+    alert(`Welcome ${username}! Use promo code "LOGIN" for 15% off.`);
     window.location.href = 'main.html';
 });
